d2b0a75d7d4f4de44668e82a5c7b87a587f1f739
Evgeny Okishev <e.okishev@lcgroup.su>
2018-11-24 23:43:56 +0700

    Лекция 4.3. Количество элементов в коллекции

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
@@ -1,12 +1,13 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 
 public class ContactDeletionTests  extends TestBase{
     @Test
     public void testContactModification(){
-
+        int before = app.getGroupHelper().getGroupCount();
         if(! app.getContactHelper().isThereAContact()) {
             app.getContactHelper().createContact(new ContactData("test1", "test2", "test3", "test4", "test5", "test6", "test17", "test8", "test9", "test9", "test10", "test11", "test12", "test13", "test14"), true);
         }
@@ -15,6 +16,8 @@ public class ContactDeletionTests  extends TestBase{
         app.getContactHelper().deleteSelectedContact();
         app.getContactHelper().closeAlert();
         app.getContactHelper().returnToContactPage();
+        int after = app.getGroupHelper().getGroupCount();
+        Assert.assertEquals(after,before - 1);
 
 
     }

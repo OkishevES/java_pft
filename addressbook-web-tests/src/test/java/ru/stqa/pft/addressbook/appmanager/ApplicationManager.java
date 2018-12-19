cea5e12f792fc1a00105fa5cc172bcea90d0dc6c
Evgeny Okishev <e.okishev@lcgroup.su>
2018-12-19 07:35:26 +0700

    Лекция 6.10. Описание конфигурации тестового стенда

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -6,18 +6,21 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 import static org.testng.Assert.fail;
 
 public class ApplicationManager {
+    private final Properties properties;
     WebDriver wd;
     public SessionHelper sessionHelper;
     public NavigationHelper navigationHelper;
     public GroupHelper groupHelper;
     public ContactHelper contactHelper;
-
-
 
     public boolean acceptNextAlert = true;
     public String baseUrl;
@@ -26,9 +29,13 @@ public class ApplicationManager {
 
     public ApplicationManager(String browser) {
         this.browser = browser;
+        properties = new Properties();
     }
 
-    public void init() {
+    public void init() throws IOException {
+        String target = System.getProperty("target", "local");
+        properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
+
         if (browser.equals(BrowserType.FIREFOX)) {
             wd = new FirefoxDriver();
         } else if (browser.equals(BrowserType.CHROME)) {
@@ -37,13 +44,13 @@ public class ApplicationManager {
             wd = new InternetExplorerDriver();
         }
         baseUrl = "https://www.katalon.com/";
-        wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
-        wd.get("http://localhost/addressbook/");
+        wd.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        wd.get(properties.getProperty("web.baseUrl"));
         groupHelper = new GroupHelper(this);
         navigationHelper = new NavigationHelper(this);
         contactHelper = new ContactHelper(this);
         sessionHelper = new SessionHelper(this);
-        sessionHelper.login("admin", "secret");
+        sessionHelper.login(properties.getProperty("web.adminLogin"), properties.getProperty("web.adminPassword"));
     }
 
     public void stop() {
@@ -54,12 +61,15 @@ public class ApplicationManager {
         }
     }
 
-    public GroupHelper group    () {
+    public GroupHelper group() {
         return groupHelper;
     }
-    public ContactHelper contact() { return contactHelper; }
+
     public NavigationHelper goTo() {
         return navigationHelper;
     }
 
+    public ContactHelper contact() {
+        return contactHelper;
+    }
 }
